9a58865cab99cfe1c2790fb26182465b8306fe9b
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-18 16:45:36 -0300

    Sistema Ecommerce

---
+++ PerriarcadoCommon/src/main/java/com/talentyco/common/entity/setting/SettingCategory.java
@@ -0,0 +1,14 @@
+package com.talentyco.common.entity.setting;
+
+/**
+ * Java Title
+ *
+ * @author Emmanuel Nieto Muñoz
+ * @version: 18/10/2021/1.0
+ * @see <a href = "" />  </a>
+ */
+
+public enum SettingCategory {
+    GENERAL, MAIL_SERVER, MAIL_TEMPLATES, CURRENCY, PAYMENT
+
+}
